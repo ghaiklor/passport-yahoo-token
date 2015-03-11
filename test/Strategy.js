47c351b70cb63f2ba4b493bcf0e63511a8b3bbf6
Eugene Obrezkov <ghaiklor@gmail.com>
2015-03-11 16:29:37 +0200

    Move profile to fixtures

--- test/Strategy.js
+++ test/Strategy.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
-    YahooTokenStrategy = require('../').Strategy;
+    YahooTokenStrategy = require('../').Strategy,
+    fakeProfile = JSON.stringify(require('./fixtures/profile.json'));
 
 describe('YahooTokenStrategy', function () {
     it('Should contain proper name', function () {
@@ -18,132 +19,7 @@ describe('YahooTokenStrategy', function () {
         });
 
         strategy._oauth2.get = function (url, accessToken, done) {
-            var body = '{ \
-                "profile": { \
-                    "uri": "https:\/\/social.yahooapis.com\/v1\/user\/12345\/profile", \
-                    "guid": "12345", \
-                    "created": "2008-08-26T23:35:16Z", \
-                    "familyName": "Edgerton", \
-                    "gender": "F", \
-                    "givenName": "Samantha", \
-                    "memberSince": "1996-10-09T01:33:06Z", \
-                    "image": { \
-                        "height": 225, \
-                        "imageUrl": "https:\/\/img.avatars.yahoo.com\/users\/1YfXUc4vMAAEB9IFDbJ_vk45UmUYE==.large.png", \
-                        "size": "150x225", \
-                        "width": 150 \
-                    }, \
-                    "interests": [ \
-                        { \
-                            "declaredInterests": [ \
-                                "Pottery", \
-                                "Tennis", \
-                                "Skiing", \
-                                "Hiking", \
-                                "Travel", \
-                                "picnics" \
-                            ], \
-                            "interestCategory": "prfFavHobbies" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "Celtic" \
-                            ], \
-                            "interestCategory": "prfFavMusic" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "Ratatouille" \
-                            ], \
-                            "interestCategory": "prfFavMovies" \
-                        }, \
-                        { \
-                            "declaredInterests": null, \
-                            "interestCategory": "prfFavFutureMovies" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "" \
-                            ], \
-                            "interestCategory": "prfFavBooks" \
-                        }, \
-                        { \
-                            "declaredInterests": null, \
-                            "interestCategory": "prfFavFutureBooks" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "" \
-                            ], \
-                            "interestCategory": "prfFavQuotes" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "Indian", \
-                                "Ethiopean" \
-                            ], \
-                            "interestCategory": "prfFavFoods" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "Britain", \
-                                "California" \
-                            ], \
-                            "interestCategory": "prfFavPlaces" \
-                        }, \
-                        { \
-                            "declaredInterests": null, \
-                            "interestCategory": "prfFavFuturePlaces" \
-                        }, \
-                        { \
-                            "declaredInterests": [ \
-                                "" \
-                            ], \
-                            "interestCategory": "prfFavAelse" \
-                        } \
-                    ], \
-                    "lang": "en-US", \
-                    "location": "Palo Alto", \
-                    "lookingFor": [ \
-                        "FRIENDSHIP", \
-                        "NETWORKING" \
-                    ], \
-                    "nickname": "Sam", \
-                    "profileUrl": "https:\/\/social.yahooapis.com\/v1/user\/profile\/usercard", \
-                    "relationshipStatus": "MARRIED", \
-                    "schools": [ \
-                        { \
-                            "id": 1, \
-                            "schoolName": "San Francisco State University", \
-                            "schoolType": "c", \
-                            "schoolYear": "2005" \
-                        }, \
-                        { \
-                            "id": 2, \
-                            "schoolName": "Univerity of Massachusetts", \
-                            "schoolType": "c", \
-                            "schoolYear": "1989" \
-                        } \
-                    ], \
-                    "status": { \
-                        "lastStatusModified": "2008-08-29", \
-                        "message": "I&#39;m working" \
-                    }, \
-                    "timeZone": "America\/Los_Angeles", \
-                    "works": [ \
-                        { \
-                            "current": true, \
-                            "id": 3, \
-                            "startDate": "2005-06-01", \
-                            "title": "Documentation Manager", \
-                            "workName": "Yahoo" \
-                        } \
-                    ], \
-                    "isConnected": true \
-                } \
-            }';
-
-            done(null, body, null);
+            done(null, fakeProfile, null);
         };
 
         strategy.userProfile('token', 'guid', function (error, profile) {
